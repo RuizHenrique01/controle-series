8ed9c37fd871c826a9057ce6a2e2c7d9e75fef34
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2024-07-20 11:16:57 -0400

    feat: Adicionando conexão com banco de dados

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,8 +1,9 @@
 <x-layout title="Criar Nova serie">
-    <form>
+    <form action="/series/salvar" method="POST">
+        @csrf
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
-            <input type="text" id="nome" class="form-control"/>
+            <input type="text" id="nome" name="nome" class="form-control"/>
         </div>
 
         <button type="submit" class="btn btn-primary">Adicionar</button>
